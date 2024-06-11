50c7f70820add618b90be812201cfbfb095df7bc
david-istvan <goerenh@gmail.com>
2024-06-11 08:26:30 +0200

    Multibib handling in the compilation flow

--- compile.py
+++ compile.py
@@ -3,7 +3,8 @@ from enum import Enum
 
 class CommandType(Enum):
     PDF = 'pdflatex',
-    BIB = 'bib'
+    BIB = 'bib',
+    MULTIBIB = 'multibib'
 
 OUTPUT_DIRECTORY = 'output'
 PDF_COMMAND = 'pdflatex'
@@ -11,12 +12,14 @@ BIB_COMMAND = 'bibtex'
 
 commands = {
     CommandType.PDF : [PDF_COMMAND, '-output-directory', OUTPUT_DIRECTORY, f'{sys.argv[1]}.tex'],
-    CommandType.BIB : [BIB_COMMAND, f'{OUTPUT_DIRECTORY}/{sys.argv[1]}.aux']
+    CommandType.BIB : [BIB_COMMAND, f'{OUTPUT_DIRECTORY}/{sys.argv[1]}.aux'],
+    # CommandType.MULTIBIB : [BIB_COMMAND, f'{OUTPUT_DIRECTORY}/{sys.argv[2]}.aux'] -- Uncomment if multibib is used (e.g., with ACM)
 }
 
 full_compile_sequence = [
     CommandType.PDF,
     CommandType.BIB,
+    # CommandType.MULTIBIB, -- Uncomment if multibib is used (e.g., with ACM)
     CommandType.PDF,
     CommandType.PDF
 ]
@@ -24,3 +27,11 @@ full_compile_sequence = [
 for c in full_compile_sequence:
     print(c)
     subprocess.call(commands[c], shell=True)
+
+"""
+Compile with:
+    python .\compile.py [main tex/bib name] ([multibib name]).
+Example:
+    python .\compile.py main -- without multibib
+    python .\compile.py main PS -- with multibib
+"""
